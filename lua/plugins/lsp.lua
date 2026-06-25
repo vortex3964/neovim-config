febaed18b38b71f4014fe051c4d84f63a77c272a
vortex3964 <theo2005.adams@gmail.com>
2026-06-25 06:19:48 +0300

    reworking the lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,41 +18,35 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			capabilities.textDocument.completion.completionItem.snippetSupport = false
 
-			local on_attach = function(_, bufnr)
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-				end
-				map("gd", vim.lsp.buf.definition, "Go to definition")
-				map("gD", vim.lsp.buf.declaration, "Go to declaration")
-				map("gr", vim.lsp.buf.references, "Go to references")
-				map("gi", vim.lsp.buf.implementation, "Go to implementation")
-				map("K", vim.lsp.buf.hover, "Hover docs")
-				map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
-				map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-				map("<leader>lf", vim.lsp.buf.format, "Format file")
-				map("[d", vim.diagnostic.goto_prev, "Prev diagnostic")
-				map("]d", vim.diagnostic.goto_next, "Next diagnostic")
-				map("<leader>ld", vim.diagnostic.open_float, "Show diagnostic")
-			end
+			-- Apply capabilities to ALL servers, before mason-lspconfig enables them
+			vim.lsp.config("*", {
+				capabilities = capabilities,
+			})
+
+			-- on_attach equivalent via LspAttach autocmd
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local bufnr = args.buf
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+					end
+					map("gd", vim.lsp.buf.definition, "Go to definition")
+					map("gD", vim.lsp.buf.declaration, "Go to declaration")
+					map("gr", vim.lsp.buf.references, "Go to references")
+					map("gi", vim.lsp.buf.implementation, "Go to implementation")
+					map("K", vim.lsp.buf.hover, "Hover docs")
+					map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+					map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+					map("<leader>lf", vim.lsp.buf.format, "Format file")
+					map("[d", vim.diagnostic.goto_prev, "Prev diagnostic")
+					map("]d", vim.diagnostic.goto_next, "Next diagnostic")
+					map("<leader>ld", vim.diagnostic.open_float, "Show diagnostic")
+				end,
+			})
 
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"ts_ls",
-					"gopls",
-					"rust_analyzer",
-					"clangd",
-					"marksman",
-					"pyright",
-				},
+				ensure_installed = { "ts_ls", "gopls", "rust_analyzer", "clangd", "marksman", "pyright" },
 				automatic_enable = true,
-				handlers = {
-					function(server_name)
-						require("lspconfig")[server_name].setup({
-							on_attach = on_attach,
-							capabilities = capabilities,
-						})
-					end,
-				},
 			})
 
 			vim.diagnostic.config({
@@ -69,4 +63,3 @@ return {
 		end,
 	},
 }
-
